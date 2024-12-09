1f50428c4429ffb631ff6affcfc060cebf385d46
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2024-12-09 10:48:39 +0000

    Test CRUD functionality

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -36,6 +36,6 @@ def edit_category(category_id):
 @app.route("/delete_category/<int:category_id>")
 def delete_category(category_id):
     category = Category.query.get_or_404(category_id)
-    db.session.delete('category')
+    db.session.delete(category)
     db.session.commit()
     return redirect(url_for("categories"))
